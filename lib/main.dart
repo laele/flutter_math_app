44b63900805a6b3a45c48b3bfa8f7d31d0b3dad8
laele <luis.sorianocrespo@outlook.com>
2026-07-03 01:16:40 -0600

    rive implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:rive/rive.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
+  await RiveNative.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final FileLoader loader;
+  @override
+  void initState() {
+    super.initState();
+    loader = FileLoader.fromAsset('assets/greg_the_frog.riv', riveFactory: Factory.rive);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    loader.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,7 +36,38 @@ class MyApp extends StatelessWidget {
       title: 'Math App',
       home: Scaffold(
         appBar: AppBar(title: const Text('Math App')),
-        body: const Center(child: Text('Hello World')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              color: Colors.red,
+              child: SizedBox(
+                width: double.infinity,
+                child: AspectRatio(
+                  aspectRatio: 16 / 9,
+                  child: RiveWidgetBuilder(
+                    fileLoader: loader,
+                    builder: (context, state) {
+                      if (state is RiveLoading) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      if (state is RiveFailed) {
+                        return Center(child: Text(state.error.toString()));
+                      }
+
+                      if (state is RiveLoaded) {
+                        return RiveWidget(controller: state.controller, fit: Fit.cover);
+                      }
+
+                      return const SizedBox();
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
